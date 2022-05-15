250b639a82997562bd7851c905e9acbe4ef3102a
nallagatlaswathi <swathivenkat1997@gmail.com>
2022-05-14 23:34:57 -0400

    006-jest-aware

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,33 @@
-const presets = [
-  [
-    '@babel/preset-env',
-    {
-      targets: {
-        edge: '17',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1',
-      },
-      useBuiltIns: 'usage',
-      corejs: '3.6.4',
-    },
-  ],
-]
+const {merge} = require('webpack-merge')
 
-module.exports = {presets}
+const config = api => {
+  const isTest = api.env('test')
+
+  console.log({isTest})
+
+  const commonConfig = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            edge: '17',
+            firefox: '60',
+            chrome: '67',
+            safari: '11.1',
+          },
+          useBuiltIns: 'usage',
+          corejs: '3.6.4',
+        },
+      ],
+    ],
+  }
+
+  if (isTest) {
+    return merge(commonConfig)
+  } else {
+    return merge(commonConfig)
+  }
+}
+
+module.exports = config
